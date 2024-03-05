120873310bff8c474999651afc464e54e665fb14
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-03-05 13:02:48 +0200

    add a button to show the calculator's history

--- src/components/CalculatorHistory.jsx
+++ src/components/CalculatorHistory.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function CalculatorHistory() {
   const calcArray = useSelector((state) => state.calculationsArray);
-  console.log(calcArray);
+  const [showHistory, setShowHistory] = useState(false);
+
+  function handleButtonClicked() {
+    setShowHistory(!showHistory);
+  }
 
   return (
     <div className="history">
-      {calcArray ? (
-        <ul>
-          {calcArray.map((el, index) => (
-            <li key={index}>{el}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No calculations available</p>
+      <button onClick={handleButtonClicked}>Show History</button>
+      {showHistory && (
+        <div>
+          {calcArray.length > 0 ? (
+            <ul>
+              {calcArray.map((el, index) => (
+                <li key={index}>{el}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No calculations available</p>
+          )}
+        </div>
       )}
     </div>
   );
